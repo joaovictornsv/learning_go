6b47b814bfa0d7e50ed2ffc62989256ba299b85a
Joao Victor Negreiros <joaovictornsv@gmail.com>
2023-02-07 16:03:47 -0300

    add comments in goroutines

--- concurrency/goroutines/goroutines.go
+++ concurrency/goroutines/goroutines.go
@@ -1,3 +1,9 @@
+/*
+Concorrência x Paralelismo
+- Concorrência: dois processos revezando o tempo de processamento
+- Paralelismo: dois processos sendo executados ao mesmo tempo
+*/
+
 package main
 
 import (
@@ -6,11 +12,12 @@ import (
 )
 
 func main() {
-	go escrever("Primeiro texto") // goroutine
-	escrever("Segundo texto")
+	// goroutine - não vai esperar essa chamada terminar para seguir com o programa
+	go escreverVariasVezes("Primeiro texto")
+	escreverVariasVezes("Segundo texto")
 }
 
-func escrever(texto string) {
+func escreverVariasVezes(texto string) {
 	for i := 0; i < 20; i++ {
 		fmt.Println(texto)
 		time.Sleep(time.Second)
